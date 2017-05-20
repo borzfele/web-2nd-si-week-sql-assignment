0f18b7904dec1881152210860d94131ea4c112da
borzfele <borzfele@gmail.com>
2017-05-20 14:40:12 +0200

    added the last two tasks

--- application_process.py
+++ application_process.py
@@ -10,6 +10,7 @@ def main_menu():
     print('4 - Find the other girl.')
     print('5 - Add the new applicant.')
     print('6 - Update Jemima\'s phone number.')
+    print('7 - Delete the application of Arsenio and his friend.')
     menu_num = input('Type the number of the menu you would like to use: ')
     return menu_num
 
@@ -65,18 +66,43 @@ def add_new_applicant():
     return dataz
 
 
+def update_phone_num():
+    connect_str = "dbname='borzfele' user='borzfele' host='localhost' password='91december30'"
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""UPDATE applicants SET phone_number = '003670/223-7459' WHERE first_name = 'Jemima' AND last_name = 'Foreman';""")
+    cursor.execute("""SELECT * FROM applicants WHERE first_name = 'Jemima' AND last_name = 'Foreman';""")
+    dataz = cursor.fetchall()
+    return dataz
+
+
+def delete_application():
+    connect_str = "dbname='borzfele' user='borzfele' host='localhost' password='91december30'"
+    conn = psycopg2.connect(connect_str)
+    conn.autocommit = True
+    cursor = conn.cursor()
+    cursor.execute("""DELETE FROM applicants WHERE email LIKE '%mauriseu.net';""")
+    return 'Applicants are successfully extermi... remo... deleted.'
+
+
 def main():
     menu_num = main_menu()
     if menu_num == '1':
         name_of_mentors()
-    if menu_num == '2':
+    elif menu_num == '2':
         nicknames_of_miskolc_mentors()
-    if menu_num == '3':
+    elif menu_num == '3':
         full_name_of_carol()
-    if menu_num == '4':
+    elif menu_num == '4':
         find_not_carol()
-    if menu_num == '5':
-        print(add_new_applicant())
+    elif menu_num == '5':
+        add_new_applicant()
+    elif menu_num == '6':
+        update_phone_num()
+    elif menu_num == '7':
+        print(delete_application())
+
 
 
 if __name__ == '__main__':
